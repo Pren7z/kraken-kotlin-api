afee8ed58076e5111af3c7be917492735a01a13c
Peter Renz <45603904+Pren7z@users.noreply.github.com>
2021-12-22 14:45:33 +0100

    Update Candlesticks.kt

--- shared/src/commonMain/kotlin/com/renzler/shared/apis/kraken/domainREST/Candlesticks.kt
+++ shared/src/commonMain/kotlin/com/renzler/shared/apis/kraken/domainREST/Candlesticks.kt
@@ -8,9 +8,9 @@ import kotlinx.serialization.json.*
 
 
 interface GetCandleList{
-    fun getError(): List<String>?
-    fun getCandleList() : JsonArray
-    fun getLastCandleTimestamp() : String
+    val getError: List<String>?
+    val getCandleSticks :  List<Candlestick>
+    val getLastCandleTimestamp : String
 
 }
 
@@ -25,20 +25,35 @@ data class Candlesticks (
 
 
     ):GetCandleList{
-    override fun getError(): List<String>?{
-        return error
+    
+    override val getError: List<String>?
+        get() = error
+
+    override val getLastCandleTimestamp
+        get() = candleSticks.values.last().toString()
+
+    override val getCandleSticks: List<Candlestick>
+        get() = getCandleList()
+
+
+    fun getCandleList(): List<Candlestick> {
+
+        val element: JsonArray = candleSticks.values.first().jsonArray
+
+        return element.map {
+            Candlestick(
+                it.jsonArray[0].toString(),
+                it.jsonArray[1].toString(),
+                it.jsonArray[2].toString(),
+                it.jsonArray[3].toString(),
+                it.jsonArray[4].toString(),
+                it.jsonArray[5].toString(),
+                it.jsonArray[6].toString(),
+                it.jsonArray[7].toString(),
+            )
+        }
     }
 
-
-    override fun getCandleList(): JsonArray {
-
-        return candleSticks.values.first().jsonArray
-    }
-
-    override fun getLastCandleTimestamp(): String {
-
-        return candleSticks.values.last().toString()
-    }
 
 }
 
